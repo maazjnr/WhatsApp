3f1a39eadd5e20c3ae4e65be8056621c4592b887
Maaz Junior <goalmaaz@gmail.com>
2022-10-14 15:17:56 +0100

    UI-3 PROPS

--- App.js
+++ App.js
@@ -2,11 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChatListItem from './src/components/ChatListItem';
 
+const chat = {
+  "id": "1",
+  "user": {
+    "image": "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+    "name": "Lukas Johan"
+  },
+
+  "lastMessage": {
+      "text": "okay, i'll see tomorrow",
+      "createdAt": "07:38"
+    }
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem />
-      <ChatListItem />
+      <ChatListItem chat={chat} />
       <StatusBar style="auto" />
     </View>
   );
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,62 +1,62 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet } from "react-native";
 
-const ChatListItem= () => {
+const ChatListItem = ({ chat }) => {
   return (
     <View style={styles.container}>
-      <Image 
-      source={{uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg"}} 
-      style={styles.Image} 
-      />
+      <Image source={{ uri: chat.user.image }} style={styles.Image} />
 
       <View style={styles.content}>
         <View style={styles.row}>
-            <Text numberOfLines={1} style={styles.name}>Lukas</Text>
-            <Text style={styles.subTitle}>8:30</Text>
+          <Text numberOfLines={1} style={styles.name}>
+            {chat.user.name}
+          </Text>
+          <Text style={styles.subTitle}>{chat.lastMessage.createdAt}</Text>
         </View>
 
-        <Text numberOfLines={2} style={styles.subTitle}>Hello there</Text>
+        <Text numberOfLines={2} style={styles.subTitle}>
+          {chat.lastMessage.text}
+        </Text>
       </View>
-
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        width: "100%",
-        paddingLeft: 10,
-        paddingRight: 10,
-        marginVertical: 5,
-        height: 70
-    },
+  container: {
+    flexDirection: "row",
+    width: "100%",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginVertical: 5,
+    height: 70,
+  },
 
-    Image: {
-        width: 60,
-        height: 60,
-        borderRadius: 30,
-        marginRight: 10
-    },
+  Image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
 
-    content: {
-        flex: 1,
-        borderBottomWidth: StyleSheet.hairlineWidth,
-        borderBottomColor: "#cccdc6"
-    },
+  content: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "#cccdc6",
+  },
 
-    row: {
-        flexDirection: "row",
-        marginBottom: 5
-    },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
 
-    name: {
-        flex: 1,
-        fontWeight: "bold"
-    },
+  name: {
+    flex: 1,
+    fontWeight: "bold",
+  },
 
-    subTitle: {
-        color: "gray"
-    }
-})
+  subTitle: {
+    color: "gray",
+  },
+});
 
-export default ChatListItem
+export default ChatListItem;
