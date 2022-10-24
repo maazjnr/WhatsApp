c28d8aa2206f35332e71bf545998968da886efb1
Maaz Junior <goalmaaz@gmail.com>
2022-10-24 23:26:50 +0100

    amending

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ChatListItem from './src/components/ChatListItem';
 import ChatsScreen from './src/screens/ChatsScreen';
 import ChatScreen from './src/screens/ChatScreen';
 import Navigator from './src/navigation';
@@ -8,7 +7,7 @@ import Navigator from './src/navigation';
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatScreen />
+      <Navigator />
       <StatusBar style='auto' />
     </View>
   );
@@ -18,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 40
   },
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,16 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import ChatsScreen from '../screens/ChatsScreen';
 import ChatScreen from '../screens/ChatScreen';
-
 
 const Stack = createNativeStackNavigator();
 
 const Navigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: 'whitesmoke' } }}>
-
+      <Stack.Navigator>
+        <Stack.Screen name="Chats" component={ChatsScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
